08ff42ed291e6764fe05d5622fed9a507f2f2cd5
RosaBen <benchabane.rosa@outlook.com>
2025-05-13 14:21:37 +0200

    ruby ex done

--- exo_17.rb
+++ exo_17.rb
@@ -2,13 +2,16 @@ print "Donnez un nombre entre 1 et 25 :"
 user_nb = gets.chomp.to_i
 
 user_nb.times do |i|
-  i += 1
-  user_nb -= 
-  character = "#"+1
-  pyramidFloor = character*i
-  spaces = " "*(user_nb -i )
-  reelPyramid = "#{spaces.to_s}#{pyramidFloor.to_s}"
+  # nb of spaces on the left from top to bottom 
+  # top line = 4 spaces + 1# => if user_nb=5 , i =0 => spaces = 5-0-1 =4
+  #2nd line = 3 spaces +3# => if user_nb=5 , i =1 => spaces = 5-1-1 =3 ...
+spaces = " "*(user_nb -i-1 )
+
+  # nb of hastags from top to bottom are odd numbers: index start at 0 so 1st line = 2times i +1 => i = 0=> 2*0+1 = 1
+  # 2nd line = 2 times i +1 => i=1 => 2*1+1 = 3
+  # 3rd line = 2 times i +1 => i=2 => 2*2+1 = 5 ... 
+  hashtags = "#" * (2 * i + 1)
+  
+  reelPyramid = "#{spaces.to_s}#{hashtags.to_s}"
   puts reelPyramid
 end
-
-# puts '#' * (2 * i + 1)
--- pyramide.rb
+++ pyramide.rb
@@ -1,11 +1,15 @@
 print "Donnez un nombre entre 1 et 25 :"
 user_nb = gets.chomp.to_i
 user_nb.times do |i|
+  # i should start from 1 instead 0 in order to create #
   i += 1
+
+  # teh n of space on the left is the userNb -1
   user_nb -= 1
-  diese = "#"
+  
+  hashtags = "#"
   space = " "*user_nb
-  normalPyramid = diese*i 
+  normalPyramid = hashtags*i 
   mirrorPyramid = "#{space}#{normalPyramid}"
   puts mirrorPyramid
 end
